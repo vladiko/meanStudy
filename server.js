eadf25cf838523d383354bf32b6a422d168e11ca
vladiko <koganvladimir@yahoo.com>
2016-04-09 23:21:00 +0300

    type script and typing directory

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var express = require('express');
+var app = express();
+app.use('/', function (req, res) {
+    res.send('Hello World');
+});
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
+module.exports = app;
