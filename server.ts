68a3d21047a07529ad165d33117540315661f0fa
vladiko <koganvladimir@yahoo.com>
2016-04-09 23:49:39 +0300

    web.config for azure deploy

--- server.ts
+++ server.ts
@@ -7,3 +7,11 @@ app.use('/', function (req, res) {
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
 module.exports = app;
+
+
+//import http = require('http');
+//var port = process.env.port || 1337
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
